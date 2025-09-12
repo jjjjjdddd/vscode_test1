ef4ec9d59939b41bf6c77df1fb8392ea228b30f0
dlekqls <ekqls8099@gmail.com>
2025-09-12 10:33:31 +0900

    Add addition and subtraction operations

---
+++ ArithmeticTest.java
@@ -0,0 +1,12 @@
+public class ArithmeticTest {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        int sum = a + b;
+        System.out.println("a + b = " + sum);
+
+        int diff = a - b;
+        System.out.println("a - b = " + diff);
+    }
+}
